c6200896b786e1b850db956cacfd88754c966f93
Mohamad Al-Zainy <zainy1453@gmail.com>
2022-01-18 20:23:00 +0200

    added print_side_by_side and display_side_by_side funcs. in Display module

--- data_checker/Dashboard.py
+++ data_checker/Dashboard.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import pandas as pd 
+import seaborn as sns
 import matplotlib.pyplot as plt 
 
 class Dashboard():
@@ -13,9 +14,25 @@ class Dashboard():
         self.data = df
    
         
-    def show_insights(self):
+    def show_preview(self):
+        
+        print("Number of columns: ", self.data.shape[1])
+        print("Number of rows: ", self.data.shape[0])
+        print('\n')
+        print('Nulls proportions: ')
+        for i,z in zip(self.data.columns,list(self.data.isnull().sum())):
+            if (z/self.data.shape[0]) != 0: 
+                print(" "*4,i,": ",round(z/self.data.shape[0],4))
+        print('\n')
+        print('Numerical columns: ')
+        print(" ",list(self.data.select_dtypes(include= ['int','float','int64','float64']).columns))
+        print('\n')
+        print('Categorical columns: ')
+        print(" ",list(self.data.select_dtypes(include= ['object']).columns))
+        print('\n')
+        print('Correlation Heatmap: ')
+        sns.heatmap(self.data.corr(),xticklabels=False,square=True, cbar = False, annot_kws = {'fontsize':12, 'fontweight': 'bold'},cmap = 'viridis_r', annot = True, fmt = '.2f');
 
-        return self.data.describe()
     
         
             
